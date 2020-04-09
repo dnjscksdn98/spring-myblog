fc4b121bed981fd3709cf8765e5c5b838c8f2d5e
Alex <dnjscksdn98@gmail.com>
2020-04-09 21:42:45 +0900

    Add: 블로그 리스트 함수 추가
    데이터베이스에 접근해서 모든 블로그를 반환해준다

--- src/main/java/com/spring/myblog/dao/ArticleDao.java
+++ src/main/java/com/spring/myblog/dao/ArticleDao.java
@@ -3,10 +3,16 @@ package com.spring.myblog.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.spring.myblog.utils.JdbcUtil;
+import com.spring.myblog.dto.ArticleDto;
 
 
 public class ArticleDao {
@@ -15,6 +21,8 @@ public class ArticleDao {
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	
+	private final String ARTICLE_LIST = "select * from articles order by rdate desc";
+	
 	public ArticleDao() {		
 		try {
 			Context context = new InitialContext();
@@ -22,5 +30,37 @@ public class ArticleDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public ArrayList<ArticleDto> list() {
+		
+		ArrayList<ArticleDto> dtos = new ArrayList<ArticleDto>();
+		
+		try {
+			conn = dataSource.getConnection();
+			
+			String query = ARTICLE_LIST;
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String writer = rs.getString("writer");
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				Timestamp rdate = rs.getTimestamp("rdate");
+				int views = rs.getInt("views");
+				
+				ArticleDto dto = new ArticleDto(id, writer, title, content, rdate, views);
+				dtos.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			JdbcUtil.closeResource(rs, pstmt, conn);
+		}
+		return dtos;
 	}
 }
