7b2a97198efe9c4aafde44235cbdef5a40dc9df2
Alex <dnjscksdn98@gmail.com>
2020-04-09 22:09:06 +0900

    Add: 블로그 글 작성 함수 추가
    작성한 블로그 글을 데이터베이스 삽입한다

--- src/main/java/com/spring/myblog/dao/ArticleDao.java
+++ src/main/java/com/spring/myblog/dao/ArticleDao.java
@@ -22,6 +22,7 @@ public class ArticleDao {
 	ResultSet rs = null;
 	
 	private final String ARTICLE_LIST = "select * from articles order by rdate desc";
+	private final String ARTICLE_WRITE = "insert into article(id, writer, title, content) values(articles_seq.nextval, ?, ?, ?)";
 	
 	public ArticleDao() {		
 		try {
@@ -62,5 +63,24 @@ public class ArticleDao {
 			JdbcUtil.closeResource(rs, pstmt, conn);
 		}
 		return dtos;
+	}
+	
+	public void write(String writer, String title, String content) {		
+		
+		try {
+			conn = dataSource.getConnection();
+			String query = ARTICLE_WRITE;
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, writer);
+			pstmt.setString(2, title);
+			pstmt.setString(3, content);
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			JdbcUtil.closeResource(pstmt, conn);
+		}
 	}
 }
